28e197090c10b3b3166373f7ae255546ed09d5df
lechan <lechan@126.com>
2021-03-17 23:29:32 +0800

    feat: 创建项目结构

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Cart from './cart'
+import Product from './product'
+
+function App() {
+  return (
+    <div>
+      <Product />
+      <Cart />
+    </div>
+  );
+}
+
+export default App
